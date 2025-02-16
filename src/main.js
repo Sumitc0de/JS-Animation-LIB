1a7c3b572ddac4d27ccf222de715483ab3f66f14
Sumit Vishwakarma <139733665+Sumitc0de@users.noreply.github.com>
2025-02-16 12:05:04 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ const cursor  = document.getElementById("cursor");
 
 
 window.addEventListener('mousemove',(dets)=>{
+    cursor.style.display = "block";
     gsap.to("#cursor",{
         x:dets.x,
         y:dets.y,
         ease: "circ.out",
     })
 })
+
